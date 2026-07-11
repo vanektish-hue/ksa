214d59414aee8c9eba117eddd735c24475fa1c91
vanektish-hue <vanektish-hue@users.noreply.github.com>
2026-07-11 19:59:13 +0300

    fix: restore babel module-resolver + metro extraNodeModules

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,23 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.alias = {
-  '@': path.resolve(__dirname, 'src'),
-};
+const projectRoot = __dirname;
+const srcDir = path.resolve(projectRoot, 'src');
+
+config.resolver.extraNodeModules = new Proxy(
+  {
+    '@': srcDir,
+  },
+  {
+    get: (target, name) => {
+      if (target.hasOwnProperty(name)) {
+        return target[name];
+      }
+      return path.join(projectRoot, 'node_modules', name);
+    },
+  },
+);
+
+config.watchFolders = [srcDir];
 
 module.exports = config;
